4018dff394836578c5e02b0a4abbbf810d2337cd
Kuzma <redkovich@gmail.com>
2015-01-28 22:55:02 +0300

    Patch for browsers which don't support html5 required option for form elements.

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -168,9 +168,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = '/home/django/django_project/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, "/api/templates"),
+    os.path.join(BASE_DIR, "api/templates"),
 )
